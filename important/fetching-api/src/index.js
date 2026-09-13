5734e6fb1f178e23d313136c353eb75ae6463c20
Muhammad Rifai <mfairendvil@gmail.com>
2026-09-13 10:43:11 +0700

    learning-post-method

--- important/fetching-api/src/index.js
+++ important/fetching-api/src/index.js
@@ -1,28 +1,50 @@
 const containerEl = document.getElementById('app')
+const formEl = document.getElementById('form-input')
 fetch("https://jsonplaceholder.typicode.com/posts")
     .then(res => res.json())
     .then(data => {
         const fragment = document.createDocumentFragment()
-
-        const titleEl = document.createElement('h1')
-        titleEl.textContent = `Blog Post`
-        fragment.appendChild(titleEl)
         
         const postsArr = data.slice(0, 5).map(post => {
             const postCard = document.createElement('div')
+            postCard.classList.add('postCard')
             
             const postTitle = document.createElement('p')
-            postTitle.classList.add('post')
+            postTitle.classList.add('postTitle')
             postTitle.textContent = post.title
             
             const postBody = document.createElement('p')
+            postBody.classList.add('postBody')
             postBody.textContent = post.body
+
+            const hrEl = document.createElement('hr')
             
-            postCard.append(postTitle, postBody)
+            postCard.append(postTitle, postBody, hrEl)
             return postCard
         })
         fragment.append(...postsArr)
         
-        containerEl.classList.add('container')
         containerEl.appendChild(fragment)
     })
+
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const formData = new FormData(formEl)
+    const postTitle = formData.get('postTitle')
+    const postBody = formData.get('postBody')
+    const data = {
+        title: postTitle,
+        body: postBody 
+    }
+    const postData = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', postData)
+        .then(res => res.json())
+        .then(data => console.log(data))
+})
